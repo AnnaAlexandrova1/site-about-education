1ffdd36c992e0816ac6d6f393116fd3e3fd4d123
Anna Aleksandrova <any7487@yandex.ru>
2022-02-14 22:18:57 +0300

    сегодня работал слайдер

--- app.js
+++ app.js
@@ -1,4 +1,38 @@
+const feedback = Array.from(document.getElementsByClassName('feedback-item'))
+const prev = Array.from(document.getElementsByClassName('slider__arrow'))
+const dots = Array.from(document.getElementsByClassName('slider__dot'))
 
-let feedback = Array.from(document.getElementsByClassName('feedback-item'))
+function whereActive(){
+    return feedback.findIndex((element) => element.classList.contains('feedback-item_active'))
+}
 
-feedback[0].classList.add('feedback-item_active')
+function whereDot(){
+    return dots.findIndex((element) => element.classList.contains('slider__dot_active'))
+}
+
+let addClass =(i) =>{
+    feedback.forEach((element) => element.classList.remove('feedback-item_active'))
+    feedback[i].classList.add('feedback-item_active')
+    dots.forEach((element) => element.classList.remove('slider__dot_active'))
+    dots[i].classList.add('slider__dot_active')
+}
+
+prev[0].onclick = () =>{
+    let i = whereActive()
+    if (i === 0){
+        i = feedback.length 
+    }
+     i--
+    addClass(i)
+}
+prev[1].onclick = () => {
+    let i = whereActive()
+    if (i === feedback.length - 1){
+        i = -1
+    }
+    i++
+     addClass(i)
+}
+dots.forEach((element, index) => element.onclick = () => {
+    addClass(index) 
+})
